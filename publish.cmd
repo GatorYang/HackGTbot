d5f39cade0bbc65f2fab8402d722f4e93c721603
Jiayang Li <gscsdyang@gmail.com>
2017-10-15 01:50:53 -0400

    first commit
    first commit.. even tho we prob finished

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=faq-chatbot-Web-Deploy.pubxml -p:Password=ozAHkCXJvjvExTLWi16JXNMSk4g2c2ThHkdwLRGfkpW7sYdmBl1nhq0EWxD0
+
